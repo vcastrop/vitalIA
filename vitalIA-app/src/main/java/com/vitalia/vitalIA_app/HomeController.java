485b90b7be49444ca4c6c123fe0a4528321f57bd
Thomas <tbuitragou@eafit.edu.co>
2025-02-10 19:00:09 -0500

    changes have been made to clear the main branch in order to work in other branches

--- vitalIA-app/src/main/java/com/vitalia/vitalIA_app/HomeController.java
+++ vitalIA-app/src/main/java/com/vitalia/vitalIA_app/HomeController.java
@@ -11,17 +11,5 @@ public class HomeController {
         return "index"; // This corresponds to "index.html"
     }
 
-    @GetMapping("/medical-history-access")
-    public String medicalHistoryAccess() {
-        System.out.println("Medical History Access page accessed");
-        return "medical-history-access"; // This corresponds to "medical-history-access.html"
-    }
-
-    @GetMapping("/medication-reminder")
-    public String medicationReminderAccess() {
-        System.out.println("Medical Reminder Access page accessed");
-        return "medication-reminder"; // This corresponds to "medical-history-access.html"
-    }
-
     // Add more methods for other user stories as needed
 }
