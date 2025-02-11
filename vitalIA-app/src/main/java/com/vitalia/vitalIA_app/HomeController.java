e7ef0196c955d2347912ce60257ac10471d536a7
vcastrop1 <vcastrop1@eafit.edu.co>
2025-02-10 19:48:10 -0500

    add menstrual-cycle-tracker

--- vitalIA-app/src/main/java/com/vitalia/vitalIA_app/HomeController.java
+++ vitalIA-app/src/main/java/com/vitalia/vitalIA_app/HomeController.java
@@ -1,6 +1,7 @@
 package com.vitalia_app;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -8,8 +9,11 @@ public class HomeController {
 
     @GetMapping("/")
     public String index() {
-        return "index"; // This corresponds to "index.html"
+        return "index"; // Main page
     }
 
-    // Add more methods for other user stories as needed
+    @GetMapping("/menstrual-cycle-tracker")
+    public String menstrual-cycle-tracker() {
+        return "menstrual-cycle-tracker"; // Menstrual cycle tracker page
+    }
 }
