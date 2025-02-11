4ed6a62d278915de91cbba7ed22fd8edb58214f0
Thomas <tbuitragou@eafit.edu.co>
2025-02-10 19:22:46 -0500

    medical-history-access and medication-reminder tabs were created in the medical-history-and-medication-reminder branch

--- vitalIA-app/src/main/java/com/vitalia/vitalIA_app/HomeController.java
+++ vitalIA-app/src/main/java/com/vitalia/vitalIA_app/HomeController.java
@@ -11,5 +11,19 @@ public class HomeController {
         return "index"; // This corresponds to "index.html"
     }
 
+    @GetMapping("/medical-history-access")
+    public String medicalHistoryAccess() {
+
+        return "medical-history-access"; // This corresponds to "medical-history-access.html"
+    }
+
+    @GetMapping("/medication-reminder")
+    public String medicationReminder() {
+
+        return "medication-reminder"; // This corresponds to "medication-reminder.html"
+    }
+
+
+
     // Add more methods for other user stories as needed
 }
