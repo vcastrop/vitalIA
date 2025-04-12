cb4850a2c7901469b33b650f063f15518e4c0936
KEPABONSA23 <kepabonn@eafit.edu.co>
2025-04-12 11:28:49 -0500

    cambios para reminder schedule for documents notification donde recordatorios que cree el usuario se guardan en el apartado donde confirmas tus documentos

--- django/documents_notification_vitalia/vitalia_app/models.py
+++ django/documents_notification_vitalia/vitalia_app/models.py
@@ -1,6 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
 
 class Paciente(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)
     nombre = models.CharField(max_length=200)
     edad = models.IntegerField()
     direccion = models.CharField(max_length=300)
@@ -13,10 +16,18 @@ class Paciente(models.Model):
 
 
 class Documento(models.Model):
-    paciente = models.ForeignKey(Paciente, on_delete=models.CASCADE)
-    tipo = models.CharField(max_length=100)
-    estado = models.CharField(max_length=50)  # Pendiente o Confirmado
+    reminder = models.ForeignKey('reminder_app.Reminder', on_delete=models.CASCADE, null=True, blank=True)
+    nombre = models.CharField(max_length=255)
+    tipo = models.CharField(max_length=50)
+    estado = models.CharField(max_length=20, default='pendiente')
+    fecha_creacion = models.DateTimeField(default=timezone.now)
+    fecha_confirmacion = models.DateTimeField(null=True, blank=True)
+
+    def confirmar(self):
+        self.estado = 'confirmado'
+        self.fecha_confirmacion = timezone.now()
+        self.save()
 
     def __str__(self):
-        return f'{self.tipo} de {self.paciente.nombre}'
+        return f"{self.nombre} - {self.estado}"
 
